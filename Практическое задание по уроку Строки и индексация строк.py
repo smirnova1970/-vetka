55e92de1c43e6ade9b384cb1a9536dfce56188fd
smirnova1970 <s.tanja.g@bk.ru>
2024-08-06 14:19:17 +0300

    Add files via upload

---
+++ Практическое задание по уроку Строки и индексация строк.py
@@ -0,0 +1,8 @@
+example = 'Топинамбур'
+print(example[0])
+print(example[-1])
+print(len(example))
+print(example[5:10])
+print(example[-1]+example[-2]+example[-3]+example[-4]+example[-5]+example[-6]+example[-7]+example[-8]+example[-9]+example[-10])
+print(example[1:10:2])
+print(''.join(reversed(example)))
