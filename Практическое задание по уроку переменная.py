7cee5e141d77a1702d21a60c87c3c744e2836f1e
smirnova1970 <s.tanja.g@bk.ru>
2024-08-07 10:35:58 +0300

    Add files via upload

---
+++ Практическое задание по уроку переменная.py
@@ -0,0 +1,5 @@
+number = 12
+hour = 1.5
+title='Python'
+average_time= hour / number
+print(title,',','всего задач:',number,', затрачено часов:',hour,', среднее время выполнения ',average_time,'часа.')
