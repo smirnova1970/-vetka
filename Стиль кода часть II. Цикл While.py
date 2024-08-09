46dda4fe02a615988617997a6892f299040914e0
smirnova1970 <s.tanja.g@bk.ru>
2024-08-09 16:32:22 +0300

    Update Стиль кода часть II. Цикл While.py

--- Стиль кода часть II. Цикл While.py
+++ Стиль кода часть II. Цикл While.py
@@ -1,9 +1,11 @@
-my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, -5]
 n = 0
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, -5]
 while n <= len(my_list):
-    if my_list[n] >= 0:
+    if my_list[n] > 0:
         print(my_list[n])
         n = n + 1
-    else:
+    elif my_list[n] == 0:
+        n = n + 1
+        continue
+    elif my_list[n] < 0:
         break
-
