47c274f26805fd508aae850adca47a09cbbd2646
smirnova1970 <s.tanja.g@bk.ru>
2024-08-09 13:03:49 +0300

    Add files via upload

---
+++ Стиль кода часть II. Цикл While.py
@@ -0,0 +1,9 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, -5]
+n = 0
+while n <= len(my_list):
+    if my_list[n] >= 0:
+        print(my_list[n])
+        n = n + 1
+    else:
+        break
+
