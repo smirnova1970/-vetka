d4975f0d621f84ad184cb7b7dddfd446c0c7d465
smirnova1970 <s.tanja.g@bk.ru>
2024-08-15 12:11:08 +0300

    Add files via upload
    В школе нам говорили, что на 0 делить нельзя. Высшая же математика опровергает это и говорит, что результат при делении на 0 будет стремиться к бесконечности.
    Давайте реализуем оба способа, чтобы у вас всегда был выбор!
    Создайте модули fake_math и true_math в которых создайте функции отвечающие за деление, но разными способами.
    В fake_math создайте функцию divide, которая принимает два параметра first и second. Функция должна возвращать результат деления first на second, но когда в second записан 0 - возвращать строку 'Ошибка'.
    В true_math создайте функцию divide, которая принимает два параметра first и second. Функция должна возвращать результат деления first на second, но когда в second записан 0 - возвращать бесконечность.
    Бесконечность можно импортировать из встроенной библиотеки math (from math import inf)

---
+++ module_4_1.py
@@ -0,0 +1,12 @@
+from fake_math import divide as mah1
+from true_math import divide as mah2
+
+
+result1 = mah1(69, 3)
+result2 = mah1(3, 0)
+result3 = mah2(49, 7)
+result4 = mah2(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
---
+++ true_math.py
@@ -0,0 +1,14 @@
+def divide(first, second):
+    if second != 0:
+        return first / second
+    else:
+       return "inf"
+
+# result1 = divide(69, 3)
+# result2 = divide(3, 0)
+# result3 = divide(49, 7)
+# result4 = divide(15, 0)
+# print(result1)
+# print(result2)
+# print(result3)
+# print(result4)
