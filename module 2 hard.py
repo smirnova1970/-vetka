2d72e028363325365bf81b2d80cb2ceb5045188a
smirnova1970 <s.tanja.g@bk.ru>
2024-08-16 11:33:19 +0300

    Update module 2 hard.py

--- module 2 hard.py
+++ module 2 hard.py
@@ -5,20 +5,13 @@ def rebus():
     number = random.choice(cod)
     return number
 result = []
-devider = []
 numbers = rebus()
 int(numbers)
-for k in range(1,21):
-    if numbers % k == 0 and k < numbers:
-        devider.append(k)
-for i in devider:
-    b = numbers - i
-    cod1 = [i,b]
-    result.append(cod1)
-for j in range(2, numbers):
-    a = numbers - i
-    cod2 = [j,a]
-    result.append(cod2)
+for i in range(1, numbers):
+    for j in range(i + 1, numbers):
+        if numbers % (i + j) == 0:
+            cod2 = [i, j]
+            result.append(cod2)
 print('Для числа: ', numbers, 'все  пароли: ', result)
 
 
