511bbe57b75654747ea84467d850f40b9dc75845
smirnova1970 <s.tanja.g@bk.ru>
2024-09-03 21:44:24 +0300

    Update module 2 hard.py

--- module 2 hard.py
+++ module 2 hard.py
@@ -15,4 +15,23 @@ for i in range(1, numbers):
 print('Для числа: ', numbers, 'все  пароли: ', result)
 
 
+# второй вариант
+import random
+
+
+def rebus():
+    cod = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    number = random.choice(cod)
+    return number
+
+
+result = ''
+numbers = rebus()
+int(numbers)
+for i in range(1, numbers):
+    for j in range(i + 1, numbers):
+        if numbers % (i + j) == 0:
+            result += str(i)
+            result += str(j)
+print('Для числа: ', numbers, 'все  пароли: ', result)
 
